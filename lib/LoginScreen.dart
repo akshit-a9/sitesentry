279689532d3d87aa11313f3bca529340f5904b36
Harshit Srivastava <69175270+subsavage@users.noreply.github.com>
2024-03-14 00:10:25 +0530

    more minor ui changes

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -10,20 +10,17 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        // Enable scrolling
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              height: 225, // Adjust this height as needed
+              height: 225,
               width: 950,
               decoration: const BoxDecoration(
-                color: Color(0xff1460D1), // The fill color of the container.
+                color: Color(0xff1460D1),
                 borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(
-                      200), // This radius should be half of the width.
-                  bottomRight: Radius.circular(
-                      200), // This radius should be half of the width.
+                  bottomLeft: Radius.circular(200),
+                  bottomRight: Radius.circular(200),
                 ),
               ),
               child: const Center(
@@ -48,31 +45,29 @@ class LoginScreen extends StatelessWidget {
                 ),
               ),
             ),
-            const SizedBox(height: 20), // Correctly placed SizedBox for spacing
+            const SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.all(20),
               child: Column(
                 children: <Widget>[
                   DropdownButtonFormField<String>(
-                    isDense: true, // This makes the dropdown smaller
+                    isDense: true,
                     decoration: InputDecoration(
-                      labelText: 'Type', // This adds a label above the dropdown
+                      labelText: 'Type',
                       contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 12,
-                          vertical: 8), // Adjust the padding to change size
+                          horizontal: 12, vertical: 8),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(30),
                       ),
                       filled: true,
                       fillColor: Colors.white,
-                      // Add drop shadow to the dropdown field
                       enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(color: Colors.transparent),
+                        borderSide: const BorderSide(color: Colors.transparent),
                       ),
                       focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(color: Colors.blue),
+                        borderSide: const BorderSide(color: Colors.blue),
                       ),
                     ),
                     value: 'Student',
@@ -80,8 +75,7 @@ class LoginScreen extends StatelessWidget {
                     items: <String>['Student', 'Faculty', 'Security', 'Admin']
                         .map<DropdownMenuItem<String>>((String value) {
                       return DropdownMenuItem<String>(
-                        alignment: Alignment
-                            .center, // Centers the text in the dropdown menu
+                        alignment: Alignment.center,
                         value: value,
                         child: Text(value),
                       );
@@ -92,37 +86,35 @@ class LoginScreen extends StatelessWidget {
                     decoration: InputDecoration(
                       hintText: 'Roll No./ Emp ID',
                       hintStyle: const TextStyle(color: Color(0xff1460D1)),
-                      border: InputBorder.none, // Removes the outline border
+                      border: InputBorder.none,
                       enabledBorder: OutlineInputBorder(
-                        // Normal state border
                         borderRadius: BorderRadius.circular(30),
                         borderSide: BorderSide.none,
                       ),
                       focusedBorder: OutlineInputBorder(
-                        // Focused state border
                         borderRadius: BorderRadius.circular(30),
                         borderSide: BorderSide.none,
                       ),
                       filled: true,
-                      fillColor: Color(0xffC2E5FF),
+                      fillColor: const Color(0xffC2E5FF),
                     ),
                   ),
                   const SizedBox(height: 20),
                   Container(
                     decoration: BoxDecoration(
-                      color: Color(0xff5BBAFF), // Use fillColor here
+                      color: const Color(0xff5BBAFF),
                       borderRadius: BorderRadius.circular(30),
                       boxShadow: [
                         BoxShadow(
                           color: Colors.black.withOpacity(0.15),
                           spreadRadius: 0,
                           blurRadius: 10,
-                          offset: Offset(0, 5), // Changes position of shadow
+                          offset: const Offset(0, 5),
                         ),
                       ],
                     ),
                     child: TextFormField(
-                      obscureText: true, // Makes it a password field
+                      obscureText: true,
                       decoration: InputDecoration(
                         hintText: 'Password',
                         hintStyle: TextStyle(color: Color(0xffFFFFFF)),
@@ -139,9 +131,7 @@ class LoginScreen extends StatelessWidget {
                           borderSide: BorderSide.none,
                         ),
                         filled: true,
-                        fillColor: const Color(
-                            0xff5BBAFF), // Ensure this matches the container's color
-                        // Do not apply boxShadow here
+                        fillColor: const Color(0xff5BBAFF),
                       ),
                     ),
                   ),
@@ -165,7 +155,7 @@ class LoginScreen extends StatelessWidget {
                         ),
                       );
                     },
-                    child: Text(
+                    child: const Text(
                       'Sign In',
                       style: TextStyle(color: Colors.white),
                     ),
@@ -176,8 +166,8 @@ class LoginScreen extends StatelessWidget {
                       ),
                       padding: const EdgeInsets.symmetric(
                           horizontal: 40, vertical: 15),
-                      shadowColor: Colors.grey, // Shadow color
-                      elevation: 6, // Shadow elevation
+                      shadowColor: Colors.grey,
+                      elevation: 6,
                     ),
                   ),
                   const SizedBox(height: 20),
@@ -202,8 +192,8 @@ class LoginScreen extends StatelessWidget {
                       ),
                       padding: const EdgeInsets.symmetric(
                           horizontal: 40, vertical: 15),
-                      shadowColor: Colors.grey, // Shadow color
-                      elevation: 6, // Shadow elevation
+                      shadowColor: Colors.grey,
+                      elevation: 6,
                     ),
                   ),
                 ],
