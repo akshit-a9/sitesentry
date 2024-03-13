bfad8796f1c58eabc422d5a6aa28b3ce33fda571
Harshit Srivastava <69175270+subsavage@users.noreply.github.com>
2024-03-14 00:06:12 +0530

    minor updates

--- lib/Student.dart
+++ lib/Student.dart
@@ -24,85 +24,87 @@ class StudentPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String name = "Simran";
+    String uid = "123456";
     return Scaffold(
-      appBar: AppBar(
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const SizedBox(height: 16),
-              Align(
-                alignment: Alignment.centerLeft, // Aligns text to the left
-                child: const Text(
-                  'Hello Simran!',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 16),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Hello $name",
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 32),
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(16),
+                const SizedBox(height: 32),
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: const Column(
+                    children: <Widget>[
+                      Text(
+                        'Scan QR Code',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Image(
+                        image: AssetImage('assets/QR.jpeg'),
+                        height: 200,
+                        width: 199,
+                      ),
+                      SizedBox(height: 16),
+                      Divider(color: Colors.white),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Your ID',
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                        cursorColor: Colors.white,
+                      ),
+                      Divider(color: Colors.white),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Entry Time',
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                        cursorColor: Colors.white,
+                      ),
+                      Divider(color: Colors.white),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Exit Time',
+                          hintStyle: TextStyle(color: Colors.white),
+                        ),
+                        style: TextStyle(color: Colors.white),
+                        cursorColor: Colors.white,
+                      ),
+                    ],
+                  ),
                 ),
-                child: const Column(
-                  children: <Widget>[
-                    const Text(
-                      'Scan QR Code',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Image(
-                      image: AssetImage('assets/QR.jpeg'),
-                      height: 200,
-                      width: 199,
-                    ), // Replace with your asset
-                    const SizedBox(height: 16),
-                    const Divider(color: Colors.white),
-                    const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Your ID',
-                        hintStyle: TextStyle(color: Colors.white),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                      cursorColor: Colors.white,
-                    ),
-                    const Divider(color: Colors.white),
-                    const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Entry Time',
-                        hintStyle: TextStyle(color: Colors.white),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                      cursorColor: Colors.white,
-                    ),
-                    const Divider(color: Colors.white),
-                    const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Exit Time',
-                        hintStyle: TextStyle(color: Colors.white),
-                      ),
-                      style: TextStyle(color: Colors.white),
-                      cursorColor: Colors.white,
-                    ),
-                  ],
-                ),
-              ),
-              // Add additional elements here if necessary
-            ],
+              ],
+            ),
           ),
         ),
       ),
